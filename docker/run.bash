0e7b4b5c90b90dae139026574ee8b3efda32d650
uu59 <k@uu59.org>
2014-05-24 17:48:23 +0900

    Remove debug code

--- docker/run.bash
+++ docker/run.bash
@@ -4,6 +4,5 @@ set -ue
 
 rails=$(cd $(dirname $0); cd ..; pwd)
 
-echo $rails
 cmd="docker run -p 8989:3000 -e DIGEST_AUTH_USER=$DIGEST_AUTH_USER -e DIGEST_AUTH_PASSWORD=$DIGEST_AUTH_PASSWORD -v $rails:/app -t kandan /app/docker/server.bash"
 $cmd || sudo $cmd
